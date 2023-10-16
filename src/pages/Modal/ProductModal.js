098da67ba5fda74f3e7b67d7666fd72d8af2b1ca
Sayan <sayand.capitalnumbers.com>
2023-10-16 18:04:01 +0530

    new design for modal

--- src/pages/Modal/ProductModal.js
+++ src/pages/Modal/ProductModal.js
@@ -1,28 +1,32 @@
 // ProductModal.js
 import React from 'react';
-import { Modal } from 'react-bootstrap';
+import { Modal, Row, Col } from 'react-bootstrap';
 
 const ProductModal = ({ show, product, onHide }) => {
-    const modalBodyStyle = {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-      };
-    
-      const imageStyle = {
-        maxWidth: '100%', // Adjust the width as needed
-        maxHeight: '300px', // Adjust the height as needed
-      };
   return (
     <Modal show={show} onHide={onHide} centered>
       <Modal.Header closeButton>
         <Modal.Title>{product.title}</Modal.Title>
       </Modal.Header>
-      <Modal.Body style={modalBodyStyle}>
-        <img src={product.images} alt={product.title} style={imageStyle} />
-        <p>{product.description}</p>
-        <p>Price: ${product.price}</p>
-        {/* Add more product details here */}
+      <Modal.Body>
+        <Row>
+          <Col md={6}>
+            <img
+              src={product.images}
+              alt={product.title}
+              style={{
+                maxWidth: '100%', // Adjust the width as needed
+                maxHeight: '300px', // Adjust the height as needed
+              }}
+            />
+          </Col>
+          <Col md={6}>
+            <strong><p>${product.title}</p></strong> 
+            <p>{product.description}</p>
+            <p>Price: ${product.price}</p>
+            {/* Add more product details here */}
+          </Col>
+        </Row>
       </Modal.Body>
     </Modal>
   );
