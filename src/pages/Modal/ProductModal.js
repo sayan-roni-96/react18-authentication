5aa2815006d85e334bacb874d30f89a4521771e1
Sayan <sayand.capitalnumbers.com>
2023-10-17 20:25:52 +0530

    new upload

--- src/pages/Modal/ProductModal.js
+++ src/pages/Modal/ProductModal.js
@@ -1,8 +1,19 @@
 // ProductModal.js
-import React from 'react';
-import { Modal, Row, Col } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Modal, Row, Col, Button } from 'react-bootstrap';
 
-const ProductModal = ({ show, product, onHide }) => {
+const ProductModal = ({ show, product, onHide, onAddToCart }) => {
+  const [quantity, setQuantity] = useState(1);
+
+  const handleIncrement = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const handleDecrement = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
   return (
     <Modal show={show} onHide={onHide} centered>
       <Modal.Header closeButton>
@@ -21,9 +32,21 @@ const ProductModal = ({ show, product, onHide }) => {
             />
           </Col>
           <Col md={6}>
-            <strong><p>${product.title}</p></strong> 
+            <strong><p>{product.title}</p></strong> 
             <p>{product.description}</p>
             <p>Price: ${product.price}</p>
+            <p>Quantity: {quantity}</p>
+            <Button variant="primary" onClick={() => onAddToCart(product, quantity)}>
+              Add to Cart
+            </Button>
+            <div className="d-flex justify-content-between mt-2">
+              <Button variant="secondary" onClick={handleDecrement}>
+                -
+              </Button>
+              <Button variant="secondary" onClick={handleIncrement}>
+                +
+              </Button>
+            </div>
             {/* Add more product details here */}
           </Col>
         </Row>
